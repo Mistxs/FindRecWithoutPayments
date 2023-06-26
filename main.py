76b21763a1923415559cf7a1da78c8c39818705d
Mistxs <96961160+Mistxs@users.noreply.github.com>
2023-06-26 10:41:12 +0300

    first beta

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ recdata = []  # массив со всеми вообще записями
 allabons = []
 alltransact = []
 allgoods = []
+allservices = []
 
 
 recstart = "2023-01-01"
@@ -48,14 +49,31 @@ def getallrec(page):
             date_string = item["date"]
             date_object = datetime.datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S')
             formatted_date = date_object.strftime('%Y-%m-%d')
+            tt_services = []
+            # print(item)
+            if "services" in item:
+                for _ in item['services']:
+                    if "id" in _:
+                        tt_services.append(_["id"])
             recdata.append({
                 "id": item["id"],
                 "client_id": item["client"]["id"],
                 "client_phone": item["client"]["phone"],
                 "date": item["date"],
+                "services": tt_services,
                 "link": f"https://yclients.com/timetable/{salon}#main_date={formatted_date}&open_modal_by_record_id={item['id']}"
             })
     return iterations
+
+def getAllServices():
+    url = f"https://api.yclients.com/api/v1/company/{salon}/services/"
+    response = requests.request("GET", url, headers=headers).json()
+    for item in response["data"]:
+        allservices.append({
+            'id': item["salon_service_id"],
+            'category_id': item['category_id'],
+            'salon': salon
+        })
 
 def parserec():
     page = 1
@@ -77,16 +95,29 @@ def parseabon():
         if response["meta"]["count"] == 0:
             break
         for item in tqdm(response["data"], desc=f"Collect abonements, page {page}"):
+            links = item['balance_container']['links']
+
+            service_container = {
+                "category": [],
+                "service": []
+            }
+            for services in links:
+                if "service" in services:
+                    if "id" in services["service"]:
+                        service_container["service"].append(services["service"]["id"])
+                if "category" in services:
+                    if "id" in services["category"]:
+                        service_container["category"].append(services["category"]["id"])
             allabons.append({
                 "id": item["id"],
                 "number": item["number"],
                 "createddate": item["created_date"],
                 "activated_date": item["activated_date"],
                 "expiration_date": item["expiration_date"],
-                "status": item["status"]["id"]
+                "status": item["status"]["id"],
+                "services": service_container
             })
         page += 1
-
 def parseLoyaltyTransaction():
     page = 1
     while True:
@@ -103,7 +134,7 @@ def parseLoyaltyTransaction():
             break
         for item in tqdm(response["data"], desc=f"Сollect loyalty transactions, page {page}"):
             alltransact.append({
-                "id": item.get("id", "['']"),
+                "id": item.get("id", ""),
                 "created_date": item.get("created_date", ''),
                 "amount": item.get("amount", ''),
                 "abonement_id": item.get("abonement_id", ''),
@@ -140,22 +171,52 @@ def parseAllGoodtrans():
         page += 1
 
 
-
-
+def insertService(dataset):
+    conn = sqlite3.connect('frwp.db')
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS services
+                      (id INTEGER PRIMARY KEY, category_id INTEGER, salon_id INTEGER)''')
+    for item in tqdm(dataset, desc=f"Insert data into the table services"):
+        try:
+            cursor.execute("INSERT INTO services VALUES (?, ?, ?)", (
+                item["id"], item["category_id"], item["salon"]))
+            conn.commit()
+        except sqlite3.IntegrityError:
+            logging.warning(f"Ошибка: услуга с id {item['id']} уже существует и не может быть добавлена в таблицу services.")
 
 def insertabon(dataset):
     conn = sqlite3.connect('frwp.db')
     cursor = conn.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS abonements
                       (id INTEGER PRIMARY KEY, number VARCHAR(100), cdate TIMESTAMP, activated TIMESTAMP, expiration TIMESTAMP, status INTEGER)''')
-    for item in tqdm(dataset, desc=f"Insert data into the table abonements"):
+    cursor.execute('''CREATE TABLE IF NOT EXISTS abonements_links (
+    id INTEGER PRIMARY KEY,
+    abonement_id INTEGER,
+    service INTEGER,
+    category INTEGER);''')
+    for item in tqdm(dataset, desc="Insert data into the table abonements"):
         try:
             cursor.execute("INSERT INTO abonements VALUES (?, ?, ?, ?, ?, ?)", (
                 item["id"], item["number"], item["createddate"], item["activated_date"], item["expiration_date"],
                 item["status"]))
             conn.commit()
+
+            categories = item["services"]["category"]
+            services = item["services"]["service"]
+
+            # Вставка данных в таблицу abonements_links для категорий
+            for i, category in enumerate(categories):
+                cursor.execute("INSERT INTO abonements_links (abonement_id, service, category) VALUES (?, ?, ?)", (
+                    item["id"], 0, category))
+            conn.commit()
+
+            # Вставка данных в таблицу abonements_links для услуг
+            for i, service in enumerate(services):
+                cursor.execute("INSERT INTO abonements_links (abonement_id, service, category) VALUES (?, ?, ?)", (
+                    item["id"], service, 0))
+            conn.commit()
         except sqlite3.IntegrityError:
-            logging.warning(f"Ошибка: запись с id {item['id']} уже существует и не может быть добавлена в таблицу abonements.")
+            logging.warning(f"Ошибка: абонемент с id {item['id']} уже существует и не может быть добавлена в таблицу abonements.")
 
 def inserttransactions(dataset):
     conn = sqlite3.connect('frwp.db')
@@ -191,10 +252,20 @@ def insertRecords(dataset):
     cursor = conn.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS records
                       (id INTEGER PRIMARY KEY, client INTEGER, date TIMESTAMP, link VARCHAR(250))''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS tt_services
+                      (id INTEGER PRIMARY KEY, record_id INTEGER, service_id INTEGER)''')
 
     for item in tqdm(dataset, desc=f"Insert data into the table records"):
         try:
             cursor.execute("INSERT INTO records VALUES (?, ?, ?, ?)", (item["id"], item["client_id"], item["date"], item["link"]))
+            conn.commit()
+
+            services = item["services"]
+
+            # Вставка данных в табицу tt_services
+            for i, service in enumerate(services):
+                cursor.execute("INSERT INTO tt_services (record_id, service_id) VALUES (?, ?)", (
+                    item["id"], service))
             conn.commit()
         except sqlite3.IntegrityError:
             logging.warning(f"Ошибка: запись с id {item['id']} уже существует и не может быть добавлена в таблицу records.")
@@ -206,27 +277,50 @@ def saveresult():
     # Выполнение SQL-запроса
     query = '''
        SELECT
-       gt.client_id AS "ID клиента",
-       gt.client_phone AS "Телефон клиента",
-       a.id AS "ID абонемента",
-       a.number as "Номер абонемента",
-       strftime('%Y-%m-%d %H:%M:%S', datetime(a.activated, 'localtime', '+1 hours')) AS "Начало действия абонемента",
-       CASE WHEN a.status = 4 THEN (SELECT strftime('%Y-%m-%d %H:%M:%S', datetime(MAX(cdate), 'localtime', '+1 hours')) FROM loyalty_transactions lt WHERE lt.abonement_id  = a.id)
-           ELSE strftime('%Y-%m-%d %H:%M:%S', datetime(a.expiration, 'localtime', '+1 hours')) END AS "Окончание действия абонемента (или дата последнего списания)",
-       CASE a.status
-           WHEN 1 THEN 'Выпущен'
-           WHEN 2 THEN 'Активен'
-           WHEN 3 THEN 'Просрочен'
-           WHEN 4 THEN 'Исчерпан'
-       END AS "Статус абонемента",
-       r.id AS "ID записи",
-       r.date AS "Дата записи",
-       r.link AS "Ссылка на визит"        
-        FROM goods_transactions gt
-        JOIN abonements a ON gt.loyalty_abonement_id = a.id
-        JOIN records r ON r.client = gt.client_id
-        WHERE gt.client_id IN (SELECT DISTINCT client FROM records)
-          AND r.date BETWEEN a.cdate AND a.expiration;
+    gt.client_id AS "ID клиента",
+    gt.client_phone AS "Телефон клиента",
+    a.id AS "ID абонемента",
+    a.number AS "Номер абонемента",
+    strftime('%Y-%m-%d %H:%M:%S', datetime(a.activated, 'localtime', '+1 hours')) AS "Начало действия абонемента",
+    CASE
+        WHEN a.status = 4 THEN (SELECT strftime('%Y-%m-%d %H:%M:%S', datetime(MAX(cdate), 'localtime', '+1 hours')) FROM loyalty_transactions lt WHERE lt.abonement_id = a.id)
+        ELSE strftime('%Y-%m-%d %H:%M:%S', datetime(a.expiration, 'localtime', '+1 hours'))
+    END AS "Окончание действия абонемента (или дата последнего списания)",
+    CASE a.status
+        WHEN 1 THEN 'Выпущен'
+        WHEN 2 THEN 'Активен'
+        WHEN 3 THEN 'Просрочен'
+        WHEN 4 THEN 'Исчерпан'
+    END AS "Статус абонемента",
+    r.id AS "ID записи",
+    r.date AS "Дата записи",
+    r.link AS "Ссылка на визит"
+FROM
+    goods_transactions gt
+    JOIN abonements a ON gt.loyalty_abonement_id = a.id
+    JOIN records r ON r.client = gt.client_id
+WHERE
+    gt.client_id IN (SELECT DISTINCT client FROM records)
+    AND r.date BETWEEN a.cdate AND a.expiration
+    AND (
+        EXISTS (
+            SELECT 1
+            FROM tt_services t
+            JOIN abonements_links al ON t.service_id = al.service
+            WHERE
+                t.record_id = r.id
+                AND al.abonement_id = a.id
+        )
+        OR EXISTS (
+            SELECT 1
+            FROM tt_services t
+            JOIN services s ON t.service_id = s.id
+            JOIN abonements_links al ON al.category = s.category_id
+            WHERE
+                t.record_id = r.id
+                AND al.abonement_id = a.id
+        )
+    );
     '''
     cursor.execute(query)
     column_headers = [description[0] for description in cursor.description]
@@ -238,7 +332,7 @@ def saveresult():
     sheet.append(column_headers)
     for row_data in results:
         sheet.append(row_data)
-    workbook.save("results.xlsx")
+    workbook.save("results_2.xlsx")
 
 
     conn.close()
@@ -251,12 +345,13 @@ def job():
     parseabon()
     parseLoyaltyTransaction()
     parseAllGoodtrans()
+    getAllServices()
     #insert into table
     insertabon(allabons)
     insertGoodTransactions(allgoods)
     inserttransactions(alltransact)
     insertRecords(recdata)
+    insertService(allservices)
     saveresult()
 
 job()
-
--- trash.py
+++ trash.py
@@ -1,6 +1,41 @@
-import datetime
+import json
 
-now = datetime.datetime.now()
-formatted_date = now.strftime('%Y-%m-%d')
+json_data = '''
+{
+    "links": [
+        {
+            "count": 0,
+            "category": {
+                "id": 10115435,
+                "category_id": 1,
+                "title": "Test 13/05"
+            }
+        },
+        {
+            "count": 0,
+            "category": {
+                "id": 10440337,
+                "category_id": 1,
+                "title": "Категория услуг"
+            }
+        }
+    ]
+}
+'''
 
-print(formatted_date)
+data = json.loads(json_data)
+
+services = {
+    "category": [],
+    "service": []
+}
+
+for link in data["links"]:
+    if "service" in link:
+        if "id" in link["service"]:
+            services["service"].append(link["service"]["id"])
+    if "category" in link:
+        if "id" in link["category"]:
+            services["category"].append(link["category"]["id"])
+
+print(services)
